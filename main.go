b2d9880271b1532f2d09b4f62d615fec8b0f17cb
Nick Loadholtes <nick@ironboundsoftware.com>
2015-09-12 16:32:42 -0400

    Formatting

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "github.com/fabioxgn/go-bot"
-    _ "github.com/fabioxgn/go-bot/commands/catfacts"
-    _ "github.com/fabioxgn/go-bot/commands/catgif"
-    _ "github.com/fabioxgn/go-bot/commands/chucknorris"
-   // Import all the commands you wish to use
-   "strings"
-   "os"
+	"github.com/fabioxgn/go-bot"
+	_ "github.com/fabioxgn/go-bot/commands/catfacts"
+	_ "github.com/fabioxgn/go-bot/commands/catgif"
+	_ "github.com/fabioxgn/go-bot/commands/chucknorris"
+	// Import all the commands you wish to use
+	"os"
+	"strings"
 )
 
 func main() {
-        bot.Run(&bot.Config{
-            Server:   os.Getenv("IRC_SERVER"),
-            Channels: strings.Split(os.Getenv("IRC_CHANNELS"), ","),
-            User:     os.Getenv("IRC_USER"),
-            Nick:     os.Getenv("IRC_NICK"),
-            Password: os.Getenv("IRC_PASSWORD"),
-            UseTLS:   true,
-            Debug:    os.Getenv("DEBUG") != "",})
+	bot.Run(&bot.Config{
+		Server:   os.Getenv("IRC_SERVER"),
+		Channels: strings.Split(os.Getenv("IRC_CHANNELS"), ","),
+		User:     os.Getenv("IRC_USER"),
+		Nick:     os.Getenv("IRC_NICK"),
+		Password: os.Getenv("IRC_PASSWORD"),
+		UseTLS:   true,
+		Debug:    os.Getenv("DEBUG") != ""})
 }
