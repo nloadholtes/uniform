5f0138936ce6d7fb6d4ee2ac17d1a071f3b4b1ae
Nick Loadholtes <nick@ironboundsoftware.com>
2015-09-12 16:18:01 -0400

    Initial code

---
+++ main.go
@@ -0,0 +1,21 @@
+    import (
+        "github.com/fabioxgn/go-bot"
+        _ "github.com/fabioxgn/go-bot/commands/catfacts"
+        _ "github.com/fabioxgn/go-bot/commands/catgif"
+        _ "github.com/fabioxgn/go-bot/commands/chucknorris"
+        // Import all the commands you wish to use
+        "log"
+        "strings"
+    )
+
+    func main() {
+        bot.Run(&bot.Config{
+            Server:   os.Getenv("IRC_SERVER"),
+            Channels: strings.Split(os.Getenv("IRC_CHANNELS"), ","),
+            User:     os.Getenv("IRC_USER"),
+            Nick:     os.Getenv("IRC_NICK"),
+            Password: os.Getenv("IRC_PASSWORD"),
+            UseTLS:   true,
+            Debug:    os.Getenv("DEBUG") != "",}
+        )
+    }
