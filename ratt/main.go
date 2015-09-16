e04fc3f57bac5b00940d552ee3e638b8688b0c9d
Nick Loadholtes <nick@ironboundsoftware.com>
2015-09-16 07:43:56 -0400

    First attempt at spitting out something

---
+++ ratt/main.go
@@ -0,0 +1,28 @@
+package ratt
+
+import (
+       "github.com/fabioxgn/go-bot"
+       "math/rand"
+       "regexp"
+)
+
+var (
+	re = regexp.MustCompile("(?i)\\b(ratt)\\b")
+	ratt_lyrics = []string {
+		    "What goes around comes around",
+		    "Dance dance dance",
+		    "Round and round",
+		    "You should know, by now"
+		    }
+)
+
+func ratt(command *bot.PassiveCmd) (string, err) {
+     if re.MatchString(command.Raw) {
+     	return ratt_lyrics[rand.Intn(len(ratt_lyrics))], nil
+	}
+	return "", nil
+}
+
+func init() {
+     bot.RegisterCommand("ratt", "Spouts out lyrics from the band Ratt")
+}
